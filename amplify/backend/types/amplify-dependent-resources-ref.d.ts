78ee9db82a03c8f8167a670c200e3863375ebcaf
lanaduffy <elanaduffy@gmail.com>
2022-08-30 23:48:26 -0400

    amplify install
    finish amplify install

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "pathfinderreact8d221df7": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
